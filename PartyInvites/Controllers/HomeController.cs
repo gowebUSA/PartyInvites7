13db9ecf1e4fead445d0af69ed41064a1f90d6c3
goric <goric@LAPTOP-T3EK1B8H>
2020-02-26 10:24:40 -0800

    Added ListResponse.cshtml

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -6,7 +6,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using PartyInvites.Models;
-using System.Linq;
 
 namespace PartyInvites.Controllers
 {
--- PartyInvites/Models/Repository.cs
+++ PartyInvites/Models/Repository.cs
@@ -7,15 +7,13 @@ namespace PartyInvites.Models
 {
     public class Repository
     {
-        private static List<GuestResponse> responses = new List<GuestResponse>();
-        public static IEnumerable<GuestResponse> Responses
+        private static List<GuestResponse> responses = new List<GuestResponse>();         //Fields  //private List<T> but any thing in this class can get to it even though it is private.
+        public static IEnumerable<GuestResponse> Responses                                  //Properties   //public property to {get}
         {
             get
-            {
-                return responses;
-            }
+            { return responses; }
         }
-        public static void AddResponse(GuestResponse response)
+        public static void AddResponse(GuestResponse response)                              //Methods      //public Add() method
         {
             responses.Add(response);
         }
