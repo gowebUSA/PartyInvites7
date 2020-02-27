e709d788d596654297b60de47a134f293fcff5d0
goric <goric@LAPTOP-T3EK1B8H>
2020-02-27 10:09:59 -0800

    Cleaned all Warnings.

--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -2,14 +2,27 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PartyInvites.Models
 {
     public class GuestResponse
     {
+        [Required(ErrorMessage = "Please enter your name")]
         public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your email adderess")]
+        [RegularExpression(".+\\@.+\\..+",
+            ErrorMessage = "Please enter your email address")]
         public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please enter your phone")]
         public string Phone { get; set; }
+
+
+        [Required(ErrorMessage = "Please specify if you are attending")]
         public bool? WillAttend { get; set; }
     }
 }
